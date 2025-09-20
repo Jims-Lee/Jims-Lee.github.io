0b9d1561426a81c7d4c07452a60eaa5c3d9cbc13
Jims <Jims@example.com>
2025-09-20 21:48:19 +0800

    upload

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/Jims-Lee.github.io/', // 添加这一行，仓库名要和你的仓库一致
-})
